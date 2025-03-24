def70f23c5fa94ab18f86610ac7f2bdf537867ca
dhanesh-yodaplus <dhanesh@yodaplus.com>
2025-03-24 16:55:33 +0530

    Second  commit for Personal Portfolio Site

--- main/views.py
+++ main/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 def home(request):
     context = {'title': 'Home', 'greeting': 'Welcome to My Portfolio!'}
-    return render(request,'home.html', context)\
+    return render(request,'home/home.html', context)\
     
 def about(request):
     context = {'title': 'About', 'description': 'I am learning Django and building a mini project!'}
-    return render(request,'about.html',context)
+    return render(request,'home/about.html',context)
 
 def skills(request):
     context = {'title': 'Skills', 'skills': ['Python','SQL','Machine Learning', 'Big Data','Django', 'Git', 'HTML', 'CSS']}
-    return render(request, 'skills.html', context)
+    return render(request, 'home/skills.html', context)
 
 def projects(request):
     context = {'title': 'Projects', 'projects': ['Personal Portfolio Site using Django','CDAC placement prediction','Image Caption Generator','Blinkit Sales Analysis using PostgreSQL','Sentiment Analysis of Amazon Reviews']}
-    return render(request, 'projects.html', context)
+    return render(request, 'home/projects.html', context)
